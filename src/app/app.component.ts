cdbfd48a469b8079b71e4f0602cdcbec563bc97f
Naveenkumar <naveenkumar.p@arisecapital.in>
2022-05-18 18:46:02 +0530

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatDatepicker} from '@angular/material/datepicker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
